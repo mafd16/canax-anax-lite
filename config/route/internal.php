94854b39fac9d7169d4400747ae39102925b2d2f
Martin Fagerlund <mngfagerlund@gmail.com>
2017-03-31 18:13:23 +0100

    It is a framework

---
+++ config/route/internal.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internal routes.
+ */
+
+
+$app->router->addInternal("404", function () use ($app) {
+    $app->view->add("take1/header", ["title" => "404"]);
+    $app->view->add("take1/navbar");
+    $app->view->add("default/404");
+
+    $app->response->setBody([$app->view, "render"])
+                  ->send(404);
+});
---
+++ src/App/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Marton\App;
+
+/**
+ * An App class to wrap the resources of the framework.
+ */
+class App
+{
+
+}
